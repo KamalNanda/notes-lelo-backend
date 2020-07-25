40528a309da1496171e28be870dc06f4f911402b
Kamal Nanda <kamalnanda45@gmail.com>
2020-07-25 22:34:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const routes = require('./routes/routes')
 const app = express()
 
 app.use(bodyParser.json())
-
+let port = process.env.PORT || 3000
 app.use('/' ,routes)
 mongoose.connect('mongodb+srv://KamalNanda:KamalNanda@cluster0-jpk3x.mongodb.net/test?retryWrites=true&w=majority' , {useNewUrlParser : true , useUnifiedTopology: true })
 .then(()=> {
-app.listen(3000, ()=>{console.log(`A Node.Js API is linstening on port 3000`)})   
+app.listen(port, ()=>{console.log(`A Node.Js API is linstening on port 3000`)})   
 })
 .catch(err => console.log(err))
 
