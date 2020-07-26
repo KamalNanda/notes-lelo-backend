ea23647bde735416d84c4ca4dd4eba9f161afc2a
Kamal Nanda <kamalnanda45@gmail.com>
2020-07-26 17:17:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const routes = require('./routes/routes')
+const cors = require("cors");
 
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.json())
 let port = process.env.PORT || 3000
 app.use('/' ,routes)
