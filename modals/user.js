a9c3b1e8e0f44fe9c34d0604e7cbf9f405826356
Kamal Nanda <kamalnanda45@gmail.com>
2020-11-30 03:57:54 +0530

    Update user.js

--- modals/user.js
+++ modals/user.js
@@ -15,6 +15,10 @@ const userSchema = new Schema({
         type : String,
         required : true
     },
+    imgUrl : {
+        type: String,
+        required : true
+    },
     course : {
         type : String,
         required : true
