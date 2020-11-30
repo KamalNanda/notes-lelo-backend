26725ee08e72bd10c866a13cc98a5450b2d2f7f6
Kamal Nanda <kamalnanda45@gmail.com>
2020-11-30 22:44:29 +0530

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,22 +1,47 @@
 const User = require('../modals/user')
 
-var admin = {
+var admin = [
+  {
+    'role': "Admin",
     'id' : "admin-1",
     'name' : "Admin",
     'email' : "admin@noteslelo.com",
     'password' : "Admin@2020"
-}
+  },
+  {
+    'role': "Intern",
+    'id' : "intern-1",
+    'name' : "Abhishek",
+    'email' : "abhishek@noteslelo.com",
+    'password' : "Intern@2020"
+  },
+  {
+    'role': "Intern",
+    'id' : "intern-2",
+    'name' : "Dilip",
+    'email' : "dilip@noteslelo.com",
+    'password' : "Intern@2020"
+  }
+]
 
 const loginAdmin = (req , res , next) => {
     const {email , password} = req.body
-
-    if(admin.email !== email){
-        return res.status(401).json("Email not found")
+    console.log(req.body)
+    let filteredAdmin = admin.filter(a => a.email === email)
+    console.log("line 31", filteredAdmin)
+    if(filteredAdmin.length === 0){
+      return res.status(401).json({status: 401, message: "Email not found!"})
     }
-    else if(admin.password !== password){
-        return res.status(401).json("Password doesn't match")
+    else{
+      let filteredAdminPassword = filteredAdmin.filter(fa => fa.password === password)
+      console.log("line 37", filteredAdminPassword)
+      if(filteredAdminPassword.length === 0){
+        return res.status(401).json({status: 401, message: "Pssword not correct!"})
+      }
+      else{
+        return res.status(200).json({status: 200, message: "Loged In", data: filteredAdminPassword[0], token: "K95990NJ17057MN98613KM95606J"})
+      }
     }
-    else res.json({message: "login" , email: email, name: admin.name, token: "K95990NJ17057MN98613KM95606J"})
 }
 
 const signUp = async (req , res , next) => {
