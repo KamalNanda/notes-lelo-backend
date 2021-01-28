6f92dba4012667600606698c92c86161b37fbd09
Kamal Nanda <kamalnanda45@gmail.com>
2021-01-28 18:35:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const app = express()
 
 app.use(cors())
 app.use(bodyParser.json())
+
+app.use((req,res,next)=>{
+    res.setHeader('Acces-Control-Allow-Origin','*');
+    res.setHeader('Acces-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Acces-Contorl-Allow-Methods','Content-Type','Authorization');
+    next(); 
+})
 let port = process.env.PORT || 3000
 app.use('/' ,routes)
 mongoose.connect('mongodb+srv://KamalNanda:KamalNanda@cluster0-jpk3x.mongodb.net/test?retryWrites=true&w=majority' , {useNewUrlParser : true , useUnifiedTopology: true })
